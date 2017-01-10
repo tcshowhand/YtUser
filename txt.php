0375a071d4d1a84356efe6e18829c1e1b8adfd0f
豫唐 <79300975@qq.com>
2017-01-10 12:55:27 +0800

    2

--- txt.php
+++ txt.php
@@ -2,17 +2,23 @@
 require 'zb_system/function/c_system_base.php';
 require 'zb_system/function/c_system_admin.php';
 
-
 $zbp->Load();
 
-
-$domain = $zbp->host;
-$userinfo = Network::Create();
-$userinfo->open('GET',"https://manage.gentie.163.com/key?url=".$domain);
-$userinfo->send();
-$userinfo = json_decode($userinfo->responseText,true);
-
-print_r($userinfo);
+$date = date('Y-m-d');
+$max = 5;
+echo '<meta http-equiv="content-type" content="text/html;charset=utf-8">';
+if(isset($_COOKIE[$date])){
+    if($_COOKIE[$date]>=$max){
+        exit('已超过'.$max.'次');
+    }else{
+        $num = $_COOKIE[$date]+1;
+        setcookie($date, $num, time()+86400);
+    }
+}else{
+    $num = 1;
+    setcookie($date, $num, time()+86400);
+}
+echo '第'.$num.'次访问';
 
 
 
